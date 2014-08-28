3abacb2e1f5a6e64acaa5637cd7e4c83df5bb947
Alireza Shafaei <alireza@shafaei.net>
2014-08-28 14:29:16 -0700

    Updated the prepare dataset script

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading the dataset!"
-wget http://www.cs.ubc.ca/~shafaei/homepage/projects/datasets/bmvc14.dataset.tar.gz
+wget -O bmvc14.dataset.tar.gz http://www.cs.ubc.ca/~shafaei/homepage/projects/datasets/bmvc14.dataset.php
 
 echo "Extracting files"
 tar xzfv bmvc14.dataset.tar.gz
